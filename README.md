475d53dbbb1788c3eb8576aef47a2838adf5719c
Carlos Andrés Planchón Prestes <carlosandresplanchonprestes@gmail.com>
2024-05-01 05:12:41 -0300

    First upload.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# gpt_pydantic_tools
-A way to write GPT tools using Pydantic Schemas.
+# GPT Pydantic Tools
+
+The `gpt_pydantic_tools` repository offers a Python module designed to integrate Pydantic models with GPT-style tool schemas. It facilitates the transformation of Pydantic models into a format that is suitable for use with GPT-4's tools functionality, ensuring structured data and tool interaction consistency.
+
+## Features
+
+- **Model Conversion:** Convert Pydantic models into GPT-4 tool schemas.
+- **Flexible Tool Choice Handling:** Provides several strategies for tool invocation such as auto, required, and none, based on the context or specific requirements.
+
+## Installation
+
+Install directly using pip:
+```bash
+pip install gpt-pydantic-tools
+```
+
+## Usage
+
+1. **Define Pydantic Models:** Create your Pydantic models as per your requirements.
+2. **Convert to GPT Tool Schema:** Use the `ToolSchemaManager` to convert your Pydantic models into GPT tool schemas.
+3. **Tool Choice Management:** Utilize the `get_tool_choice_dict` to manage how tools are chosen for execution based on the defined strategies.
+
+Example usage:
+```python
+from pydantic import BaseModel
+from gpt_pydantic_tools import ToolSchemaManager, get_tool_choice_dict, ToolChoiceEnum
+
+class MyModel(BaseModel):
+    name: str
+    age: int
+
+# Convert Pydantic model to GPT tool schema
+schema_manager = ToolSchemaManager(pydantic_obj=MyModel)
+tool_schema = schema_manager.tools_schema
+
+# Determine tool invocation strategy
+tool_choice = get_tool_choice_dict(ToolChoiceEnum.AUTO, schema_manager)
+```
+
+## Contributing
+
+Contributions are welcome! Please feel free to submit pull requests, create issues, and suggest improvements to the repository.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ gpt_pydantic_tools/__init__.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+
+import attrs
+from attrs_strict import type_validator
+
+from pydantic import BaseModel
+
+import jsonschema
+
+# https://github.com/pydantic/pydantic/issues/6381
+"""
+ModelMetaclass is not a public class.
+They want to be able to refactor the ModelMetaclass
+without it being considered a breaking change.
+"""
+from pydantic._internal._model_construction import ModelMetaclass
+
+from enum import StrEnum
+
+from typing import Any
+
+
+def remove_key_from_dict(
+    dict_obj: dict[str, Any],
+    key_to_remove: str
+        ) -> dict[str, Any]:
+    """
+    Recursively remove the specified key from a dictionary.
+
+    :param dict_obj: Dictionary from which the key should be removed
+    :param key_to_remove: Key that needs to be removed
+    :return: Dictionary without the specified key
+    """
+    if isinstance(dict_obj, dict):
+        return {
+            k: remove_key_from_dict(
+                dict_obj=v, key_to_remove=key_to_remove
+            ) for k, v in dict_obj.items() if k != key_to_remove
+        }
+    elif isinstance(dict_obj, list):
+        return [
+            remove_key_from_dict(
+                dict_obj=item, key_to_remove=key_to_remove
+            ) for item in dict_obj
+        ]
+    else:
+        return dict_obj
+
+
+###########################################
+#                                         #
+#   --- PYDANTIC OBJ TO TOOL SCHEMA ---   #
+#                                         #
+###########################################
+ToolsSchemaT = list[dict[str, Any]]
+
+
+def pydantic_obj_to_tool_schema(
+    pydantic_obj: BaseModel,
+    description: str = None
+        ) -> ToolsSchemaT:
+    json_data = pydantic_obj.schema()
+
+    func_name: str = json_data["title"]
+
+    gpt_function_dict = remove_key_from_dict(
+        dict_obj=json_data,
+        key_to_remove="title"
+    )
+
+    if "description" in gpt_function_dict.keys():
+        description: str = gpt_function_dict.pop("description")
+    else:
+        description = description
+
+    tool_dict = {
+        "name": func_name,
+        "description": description,
+        "parameters": gpt_function_dict
+    }
+
+    tools_schema = [
+        {
+            "type": "function",
+            "function": tool_dict
+        }
+    ]
+    return tools_schema
+
+
+@attrs.define()
+class ToolSchemaManager:
+    pydantic_obj: ModelMetaclass = attrs.field(validator=type_validator())
+
+    tools_schema: ToolsSchemaT = attrs.field(
+        validator=type_validator(),
+        init=False
+    )
+
+    tool_name: str = attrs.field(
+        validator=type_validator(),
+        init=False
+    )
+
+    description: str = attrs.field(
+        validator=type_validator(),
+        default=""
+    )
+
+    def __attrs_post_init__(self):
+        self.tools_schema = pydantic_obj_to_tool_schema(
+            pydantic_obj=self.pydantic_obj,
+            description=self.description
+        )
+
+        self.tool_name = self.tools_schema[0]["function"]["name"]
+
+    def validate_tool_answer(
+        self,
+        schema_to_validate,
+            ) -> bool | jsonschema.exceptions.ValidationError:
+
+        pydantic_obj_json_schema = self.pydantic_obj.schema()
+        try:
+            jsonschema.validate(
+                instance=schema_to_validate,
+                schema=pydantic_obj_json_schema
+            )
+            # print("JSON data is valid.")
+            return True
+        except jsonschema.exceptions.ValidationError as err:
+            # print("JSON data is invalid.")
+            return err
+
+
+#####################################
+#                                   #
+#   --- TOOL CHOICE PARAMETER ---   #
+#                                   #
+#####################################
+ToolChoiceT = dict[str, Any] | str
+
+
+class ToolChoiceEnum(StrEnum):
+    AUTO: str = "auto"
+    REQUIRED: str = "required"
+    NONE: str = "none"
+    TOOL_NAME: str = "tool_name"
+
+
+def get_tool_choice_dict(
+    tool_choice: ToolChoiceEnum,
+    schema_manager: ToolSchemaManager
+        ) -> ToolChoiceT:
+    match tool_choice:
+        case ToolChoiceEnum.AUTO:
+            return "auto"
+        case ToolChoiceEnum.REQUIRED:
+            return "required"
+        case ToolChoiceEnum.NONE:
+            return "none"
+        case ToolChoiceEnum.TOOL_NAME:
+            tool_choice_value = {
+                "type": "function",
+                "function": {
+                    "name": schema_manager.tool_name
+                }
+            }
+        case _:
+            raise ValueError(f"Invalid tool choice: {tool_choice}")
+
+    return tool_choice_value
